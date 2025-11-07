03f655576ee2e777d667ef20a2b6cdf06457bc6f
Brad <levinskb@sheridancollege.ca>
2025-11-07 11:04:48 -0500

    Update tests.py

--- .github/workflows/tests.py
+++ .github/workflows/tests.py
@@ -1,2 +1,11 @@
-print("hello World")
-print(" This is me pushing to see if it works")
+# .github/workflows/tests.py
+import unittest
+
+class My_Test(unittest.TestCase):
+    def test_my_Test(self):  # unittest requires 'test_' prefix
+        print("hello World")
+        print(" This is me pushing to see if it works")
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
